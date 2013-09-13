0d58f1c14a534f170ab1dd5448755fe34cb6ea99
Josh Bruce <josh@surftrack.co.uk>
2013-09-13 10:57:36 +0100

    Implement the drain and tap gesture for saving food from the drain

--- KitchenSink/KitchenSinkViewController.m
+++ KitchenSink/KitchenSinkViewController.m
@@ -10,10 +10,72 @@
 #import "AskerViewController.h"
 
 @interface KitchenSinkViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *kitchenSink;
 @end
 
 @implementation KitchenSinkViewController
+
+#define DRAIN_DURATION 3.0
+#define DRAIN_DELAY 1.0
+
+- (void)drain
+{
+    for (UIView *view in self.kitchenSink.subviews) {
+        CGAffineTransform transform = view.transform;
+        if (CGAffineTransformIsIdentity(transform)) {
+            [UIView animateWithDuration:DRAIN_DURATION / 3 delay:DRAIN_DELAY options:UIViewAnimationOptionCurveLinear animations:^{
+                view.transform = CGAffineTransformRotate(CGAffineTransformScale(transform, 0.7, 0.7), 2 * M_PI / 3);
+            } completion:^(BOOL finished) {
+                if (finished) [UIView animateWithDuration:DRAIN_DURATION / 3 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+                    view.transform = CGAffineTransformRotate(CGAffineTransformScale(transform, 0.4, 0.4), -2 * M_PI / 3);
+                } completion:^(BOOL finished) {
+                    if (finished) [UIView animateWithDuration:DRAIN_DURATION / 3 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+                        view.transform = CGAffineTransformScale(transform, 0.1, 0.1);
+                    } completion:^(BOOL finished) {
+                        if (finished) [view removeFromSuperview];
+                    }];
+                }];
+            }];
+        }
+    }
+}
+
+#define MOVE_DURATION 3.0
+
+- (IBAction)tap:(UITapGestureRecognizer *)sender
+{
+    CGPoint tapLocation = [sender locationInView:self.kitchenSink];
+    for (UIView * view in self.kitchenSink.subviews) {
+        if (CGRectContainsPoint(view.frame, tapLocation)) {
+            [UIView animateWithDuration:MOVE_DURATION delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                [self setRandomLocationForView:view];
+                view.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.99, 0.99);
+            } completion:^(BOOL finished) {
+                view.transform = CGAffineTransformIdentity;
+            }];
+        }
+    }
+}
+
+- (void)addFood:(NSString *)food
+{
+    UILabel *foodLabel = [[UILabel alloc] init];
+    foodLabel.text = food;
+    foodLabel.font = [UIFont systemFontOfSize:46];
+    foodLabel.backgroundColor = [UIColor clearColor];
+    [self setRandomLocationForView:foodLabel];
+    [self.kitchenSink addSubview:foodLabel];
+    [self drain];
+}
+
+- (void)setRandomLocationForView:(UIView *)view
+{
+    [view sizeToFit];
+    CGRect sinkBounds = CGRectInset(self.kitchenSink.bounds, view.frame.size.width / 2, view.frame.size.height / 2);
+    CGFloat x = arc4random() % (int)sinkBounds.size.width + view.frame.size.width / 2;
+    CGFloat y = arc4random() % (int)sinkBounds.size.height + view.frame.size.height / 2;
+    view.center = CGPointMake(x, y);
+}
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -30,7 +92,7 @@
 - (IBAction)doneAsking:(UIStoryboardSegue *)segue
 {
     AskerViewController *asker = segue.sourceViewController;
-    NSLog(@"%@", asker.answer);
+    [self addFood:asker.answer];
 }
 
 @end
